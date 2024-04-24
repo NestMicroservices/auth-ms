a16fcbed36777612b80f8b3cfbbcce523c32deda
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-04-24 10:30:19 -0500

    Login user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
 import { bcryptAdapter, Services } from 'src/config';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -34,8 +34,43 @@ export class AuthService extends PrismaClient implements OnModuleInit {
       const user = await this.user.create({
         data: { email, name, password: await bcryptAdapter.hash(password) },
       });
-      const { password: _, ...rest } = user;
 
+      const { password: _, ...rest } = user;
+      return { user: rest, token: 'token' };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    try {
+      const user = await this.user.findUnique({
+        where: { email: loginUserDto.email },
+      });
+
+      if (!user) {
+        throw new RpcException({
+          status: 401,
+          message: 'Invalid credentials',
+        });
+      }
+
+      const isAuth = await bcryptAdapter.compare(
+        loginUserDto.password,
+        user.password,
+      );
+
+      if (!isAuth) {
+        throw new RpcException({
+          status: 401,
+          message: 'Invalid credentials',
+        });
+      }
+
+      const { password: _, ...rest } = user;
       return { user: rest, token: 'token' };
     } catch (error) {
       throw new RpcException({
